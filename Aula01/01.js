3518dc22f200d84eeb26e951737d87d7e85b14a4
Joel Santos <joeloliveira56@gmail.com>
2025-05-07 14:50:30 -0300

    Adicionando arquivos da Aula 01
    Nessa aula estudamos o conteúdo inicial de JavaScript.

---
+++ Aula01/01.js
@@ -0,0 +1,13 @@
+var mensagem = "Hello World!!!";
+var x = 90;
+console.log(mensagem);
+
+let nota01 = 9, nota02=8, nota03=7, nota04=4;
+let media  = (nota01+nota02+nota03+nota04)/4;
+console.log("A média é:" + media);
+
+
+
+
+
+
